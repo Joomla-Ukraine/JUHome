1ec4c1a082d9b0643632ded798601b203358a9d8
Denys N <denys@joomla-ua.org>
2018-01-03 05:56:46 +0200

    Change copyright

--- controller.php
+++ controller.php
@@ -5,7 +5,7 @@
  * @version        3.x
  * @package        JUHome
  * @author         Denys D. Nosov (denys@joomla-ua.org)
- * @copyright (C)  2011-2017 by Denys D. Nosov (https://joomla-ua.org)
+ * @copyright (C)  2011-2018 by Denys D. Nosov (https://joomla-ua.org)
  * @license        GNU General Public License version 2 or later
  *
  **/
--- views/home/tmpl/default.php
+++ views/home/tmpl/default.php
@@ -5,7 +5,7 @@
  * @version        3.x
  * @package        JUHome
  * @author         Denys D. Nosov (denys@joomla-ua.org)
- * @copyright (C)  2011-2017 by Denys D. Nosov (https://joomla-ua.org)
+ * @copyright (C)  2011-2018 by Denys D. Nosov (https://joomla-ua.org)
  * @license        GNU General Public License version 2 or later
  *
  **/
@@ -15,7 +15,7 @@ defined('_JEXEC') or die;
 
 if($params->get('display_title'))
 {
-	echo '<h1 class="head">' . $params->get('page_title') . '</h1>';
+	echo '<h1>' . $params->get('page_title') . '</h1>';
 }
 
 echo HomeViewHome::LoadPositionForHome($home->mod_name_pref, $style = ($home->style ? $home->style : 'raw'));
